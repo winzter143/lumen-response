a97dbe482b6220163d07fca79e7f34bf2e7a5b86
Paolo Basa <paolobasa@gmail.com>
2016-09-29 10:25:31 +0800

    Add helper functions for fetching, assigning, and revoking party roles.

--- src/Components/RBAC.php
+++ src/Components/RBAC.php
@@ -1,17 +1,47 @@
 <?php
 namespace F3\Components;
 
+use DB;
+
 /**
  * RBAC trait.
  */
 trait RBAC
 {
     /**
+     * Returns the roles assigned to the party.
+     */
+    public function getRoles()
+    {
+        // Get the roles.
+        $roles = DB::table('core.party_roles as pr')
+            ->select('r.name', 'r.permissions')
+            ->join('core.roles as r', 'r.id', '=', 'pr.role_id')
+            ->where([['pr.party_id', $this->party_id]])
+            ->pluck('permissions', 'name')
+            ->toArray();
+
+        if ($roles) {
+            // Decode the permissions.
+            $roles = array_map(function($permission) {
+                return json_decode($permission);
+            }, $roles);
+        } else {
+            $roles = [];
+        }
+
+        return $roles;
+    }
+
+    /**
      * Checks if the role is assigned to the user.
      * @param string|array $roles
      */
     public function hasRole($roles)
     {
+        // Get the user roles.
+        $user_roles = $this->getRoles();
+
         // Convert $roles to an array.
         if (!is_array($roles)) {
             $roles = [$roles];
@@ -19,7 +49,7 @@ trait RBAC
 
         // Check if the user is assigned the role.
         foreach ($roles as $role) {
-            if (in_array($role, array_keys($this->roles))) {
+            if (in_array($role, array_keys($user_roles))) {
                 return true;
             }
         }
@@ -34,6 +64,9 @@ trait RBAC
      */
     public function can($permissions)
     {
+        // Get the user roles.
+        $user_roles = $this->getRoles();
+
         // Convert $permissions to an array.
         if (!is_array($permissions)) {
             $permissions = [$permissions];
@@ -41,13 +74,72 @@ trait RBAC
 
         // Check if the user is assigned the permission.
         foreach ($permissions as $permission) {
-            if (in_array($permission, array_flatten($this->roles))) {
+            if (in_array($permission, array_flatten($user_roles))) {
                 return true;
             }
         }
 
         // None of the permissions are assigned to the user.
         return false;
+    }
+
+    /**
+     * Revokes a party role.
+     */
+    public function revokeRole($role)
+    {
+        // Check if the role exists.
+        $role = DB::table('core.roles')->where('name', $role)->first();
+
+        if (!$role) {
+            throw new \Exception('The role does not exist.');
+        }
+
+        // Delete the role.
+        return DB::table('core.party_roles')->where([['party_id', $this->party_id], ['role_id', $role['id']]])->delete();
+    }
+
+    /**
+     * Assigns a role to the party.
+     */
+    public function assignRole($role)
+    {
+        // Check if the role exists.
+        $role = DB::table('core.roles')->where('name', $role)->first();
+
+        if (!$role) {
+            throw new \Exception('The role does not exist.');
+        }
+
+        // Check if the party exists and if it's active.
+        $party = DB::table('core.parties')->where([['id', $this->party_id], ['status', 1]])->first();
+
+        if (!$party) {
+            throw new \Exception('The party does not exist or may have been disabled.');
+        }
+
+        // Check if the mapping exists.
+        $result = DB::table('core.party_roles')->where([['party_id', $party['id']], ['role_id', $role['id']]])->first();
+
+        if ($result) {
+            // The mapping exists.
+            return true;
+        }
+
+        try {
+            // The mapping does not exist. Assign the role to the party.
+            return DB::table('core.party_roles')->insert([
+                ['party_id' => $party['id'], 'role_id' => $role['id']]
+            ]);
+        } catch (\Exception $e) {
+            // Multiple inserts might still happen even if we checked earlier if the mapping exists.
+            // Check if it's a constraint error.
+            if ($e->getCode() == Model::PG_ERROR_UNIQUE_VIOLATION) {
+                return true;
+            } else {
+                throw $e;
+            }
+        }
     }
 
     /**
